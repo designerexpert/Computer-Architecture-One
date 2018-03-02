3c3c7dcc97b7812915a78b9927ba774bf42ebca4
Moises Dobarganes <designerexpert@gmail.com>
2018-03-01 22:32:00 -0500

    Implemented a few more instructions, began work on Stack

--- project/ls8/cpu.js
+++ project/ls8/cpu.js
@@ -4,16 +4,32 @@
 
 const fs = require('fs');
 
+// Stack Creation
+
+
 // Instructions
 
-const HLT = 0b00000001; // 1 Halt CPU
-// !!! IMPLEMENT ME
-// LDI
-const LDI = 0b10011001; // 153 Set the value of a register to an integer.
-// MUL
-const MUL = 0b10101010; // Multiply two registers together and store the result in registerA.
-// PRN
-const PRN = 0b01000011; // Print numeric value stored in the given register.
+const SP = 7; // STACK POINTER          | Stack starts at F4 memory location: 244.
+const IS = 6; // INTERRUPT STATUS       | Reserved Register Location for Interrupt Status
+const IM = 5; // INTERRUPT MASK         | Reserved Register Location for Interrupt Mask
+
+const FL_EQ = 0b00000001; // FLAG X = Y | Equality Flag: EQUAL.
+const FL_GT = 0b00000010; // FLAG X > Y | Equality Flag: Greater Than.
+const FL_LT = 0b00000100; // FLAG X < Y | Equality Flag: Less Than.
+
+const HLT = 0b00000001; // HLT          | Halt CPU.
+const LDI = 0b10011001; // LDI REG      | Save the value given inside a register.
+const MUL = 0b10101010; // MUL REG REG  | Multiply two registers together and store the result in registerA.
+const PRN = 0b01000011; // PRN REG      | Print numeric value stored in the given register.
+const ADD = 0b10101000; // ADD REG REG  | Adds two registers and saves the result in the first.
+const AND = 0b10110011; // AND REG REG  | Bitwise-AND registerA and registerB, then store the result in registerA.
+const NOP = 0b00000000; // NOP          | No operation. Do nothing for this instruction.
+const NOT = 0b01110000; // NOT REG      | Performs a bitwise-NOT on the value in the provided register.
+
+// --------- NEED TO IMPLEMENT
+const CMP = 0b10100000; // CMP REG REG  | Compare the value in two registers. Sets Appropriate Flag in reg.FL bitwise.
+const PUSH = 0b01001101; // PUSH REG    | Decrement the SP, Copy Reg Value to location pointed by the new SP.
+const POP = 0b00001001; // POP R        | Copy Value pointed by SP to Reg, Increment the SP.
 /**
  * Class for simulating a simple Computer (CPU & memory)
  */
@@ -28,8 +44,12 @@ class CPU {
         this.reg = new Array(8).fill(0); // General-purpose registers
 
         // Special-purpose registers
-        this.reg.PC = 0; // Program Counter
-        this.reg.IR = 0; // Instruction Register
+        this.reg.PC = 0; // Program Counter.
+        this.reg.IR = 0; // Instruction Register.
+        this.reg.FL = 0; // Comparison Flag FL bits: 00000LGE.
+        this.reg[SP] = this.ram.read(0xF4); //INITIALIZE Stack Pointing to Memory.
+        this.reg[IS] = 0; // INITIALIZE Interrupt Status on Register.
+        this.reg[IM] = 0; // INITIALIZE Interrupt Mask on Register.
 
         this.setupBranchTable();
     }
@@ -41,13 +61,18 @@ class CPU {
         let bt = {};
 
         bt[HLT] = this.HLT;
-        // !!! IMPLEMENT ME
-        // LDI
         bt[LDI] = this.LDI;
-        // MUL
         bt[MUL] = this.MUL;
-        // PRN
         bt[PRN] = this.PRN;
+        bt[ADD] = this.ADD;
+        bt[AND] = this.AND;
+        bt[NOP] = this.NOP;
+        bt[NOT] = this.NOT;
+
+        // --------- NEED TO IMPLEMENT
+        bt[CMP] = this.CMP;
+        bt[PUSH] = this.PUSH;
+        bt[POP] = this.POP;
 
         this.branchTable = bt;
     }
@@ -77,6 +102,14 @@ class CPU {
         clearInterval(this.clock);
     }
 
+    setFlag(flag, value) {
+        if (value === true) {
+            this.reg.FL = this.reg.FL | flag;
+        } else {
+            this.reg.FL = this.reg.FL & (~flag); // The Inverse of the Flag is obtained using ~
+        }
+    }
+
     /**
      * ALU functionality
      * 
@@ -85,8 +118,19 @@ class CPU {
     alu(op, regA, regB) {
         switch (op) {
             case 'MUL':
-                // !!! IMPLEMENT ME
                 this.reg[regA] = (this.reg[regA] * this.reg[regB]) & 255;
+                break;
+            case 'ADD':
+                this.reg[regA] = (this.reg[regA] + this.reg[regB]) & 255;
+                break;
+            case 'AND':
+                this.reg[regA] = (this.reg[regA] & this.reg[regB]) & 255;
+                break;
+            case 'NOT':
+                this.reg[regA] = (~this.reg[regA]) & 255;
+                break;
+            case 'CMP':
+                this.setFlag(FL_EQ, this.reg[regA] === this.reg[regB]);
                 break;
         }
     }
@@ -96,22 +140,17 @@ class CPU {
      */
     tick() {
         // Load the instruction register (OR) from the current PC
-        // !!! IMPLEMENT ME
-        // console.log('This.reg.pc = ', this.reg.PC)
-        // console.log('ramread', this.ram.read(this.reg.PC))
         this.reg.IR = this.ram.read(this.reg.PC) // Use the PC counter to load the appropriate Instruction from Ram into the Register.
 
         // Debugging output
-        console.log(`${this.reg.PC}: ${this.reg.IR.toString(2)}`);
+        // console.log(`${this.reg.PC}: ${this.reg.IR.toString(2)}`);
 
         // Based on the value in the Instruction Register, locate the
         // appropriate hander in the branchTable
-        // !!! IMPLEMENT ME
         let handler = this.branchTable[this.reg.IR];
 
         // Check that the handler is defined, halt if not (invalid
         // instruction)
-        // !!! IMPLEMENT ME
         if (handler === undefined) {
             console.log(`Unknown code: ${this.reg.IR}`);
             this.stopClock(); // Exit with Error
@@ -125,43 +164,52 @@ class CPU {
         handler.call(this, operandA, operandB);
 
         // Increment the PC register to go to the next instruction
-        // !!! IMPLEMENT ME
         this.reg.PC += ((this.reg.IR >> 6) & 0b00000011) + 1;
     }
 
     // INSTRUCTION HANDLER CODE:
 
-    /**
-     * HLT
-     */
     HLT() {
-        // !!! IMPLEMENT ME
         this.stopClock();
     }
 
-    /**
-     * LDI R,I
-     */
     LDI(regNum, value) {
-        // !!! IMPLEMENT ME
         this.reg[regNum] = value & 255; // Coerce value to 255 max.
     }
 
-    /**
-     * MUL R,R
-     */
     MUL(regA, regB) {
-        // !!! IMPLEMENT ME
-        // Call the ALU
         this.alu('MUL', regA, regB);
     }
 
-    /**
-     * PRN R
-     */
     PRN(regA) {
-        // !!! IMPLEMENT ME
         console.log(this.reg[regA])
+    }
+
+    ADD(regA, regB) {
+        this.alu('ADD', regA, regB);
+    }
+
+    AND(regA, regB) {
+        this.alu('AND', regA, regB);
+    }
+
+    NOT(regA) {
+        this.alu('NOT', regA);
+    }
+
+    NOP() {
+        return;
+    }
+
+    CMP(regA, regB) {
+        this.alu('CMP', regA, regB);
+    }
+
+    PUSH() {
+
+    }
+    POP() {
+
     }
 }
 
